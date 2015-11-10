da00039a2172da860edfa7fe718f3e3f1b2ccfa3
Erik Christianson <internet.erik@gmail.com>
2015-11-10 16:41:08 -0600

    adding some collections

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,18 @@
 import {loadCharacters} from 'server/loadCharacters';
 
 import {Rolls} from 'collections/rolls';
+import {Spells} from 'collections/spells';
+import {Skills} from 'collections/skills';
+import {Feats} from 'collections/feats';
 
 Meteor.startup(function() { 
+		//for some reason if we don't do something with the DBS on start, 
+		//they won't show on the front end
 		loadCharacters();
-		Rolls.find().count(); 
+		Rolls.find().count();
+		Spells.find().count();
+		Skills.find().count();
+		Feats.find().count(); 
 });
 
 Meteor.methods({

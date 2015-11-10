d4faebe3eeedbd9347d0c8bbea93d5ce7344459b
Erik Christianson <internet.erik@gmail.com>
2015-11-10 17:41:37 -0600

    adding a collection for monsters

--- server/main.ts
+++ server/main.ts
@@ -4,6 +4,7 @@ import {Rolls} from 'collections/rolls';
 import {Spells} from 'collections/spells';
 import {Skills} from 'collections/skills';
 import {Feats} from 'collections/feats';
+import {Monsters} from 'collections/monsters';
 
 Meteor.startup(function() { 
 		//for some reason if we don't do something with the DBS on start, 
@@ -12,7 +13,8 @@ Meteor.startup(function() {
 		Rolls.find().count();
 		Spells.find().count();
 		Skills.find().count();
-		Feats.find().count(); 
+		Feats.find().count();
+		Monsters.find().count(); 
 });
 
 Meteor.methods({

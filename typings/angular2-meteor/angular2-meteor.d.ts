29452cb12c08a30c386714766652c69b81d25506
Erik Christianson <internet.erik@gmail.com>
2016-01-29 11:30:32 -0600

    moving add character button

---
+++ typings/angular2-meteor/angular2-meteor.d.ts
@@ -0,0 +1,18 @@
+
+/// <reference path="../angular2/angular2.d.ts" />
+/// <reference path="../meteor/meteor.d.ts" />
+
+declare module ngMeteor {
+  class MeteorComponent {
+    subscribe(name: string, ...rest: any[]): Meteor.SubscriptionHandle;
+    autorun(runFunc: Function, autoBind?: boolean): Tracker.Computation;
+    call(name: string, ...rest: any[]);
+    ngOnDestroy():void;
+  }
+
+  function bootstrap(appComponentType: /*Type*/ any, bindings?: Array<core.Type | core.Provider | any[]>): Promise<core.ApplicationRef>;
+}
+
+declare module "angular2-meteor" {
+  export = ngMeteor;
+}

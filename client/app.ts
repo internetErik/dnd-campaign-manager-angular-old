5a2e7b4f4fc35005eb67de577845a5b75ab4f495
Erik Christianson <internet.erik@gmail.com>
2016-05-10 11:27:57 -0500

    update to meteor version

--- client/app.ts
+++ client/app.ts
@@ -1,26 +1,23 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import {Component, provide} from 'angular2/core';
-
-import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, APP_BASE_HREF} 
-	from 'angular2/router';
-
-import {bootstrap} from 'angular2-meteor-auto-bootstrap';
-
-import {Navigation} from './shared/navigation/navigation';
-import {DiceHelper} from './shared/dice-helper/dice-helper';
-import {CommandPalette} from './shared/command-palette/command-palette';
-
-import {HomePage} from './pages/home-page/home-page';
-import {CampaignList} from './pages/campaign-list/campaign-list';
-import {CampaignForm} from './pages/campaign-form/campaign-form';
-import {ContentCreator} from './pages/content-creator/content-creator';
-import {CharacterList} from './pages/character-list/character-list';
-import {CharacterForm} from './pages/character-form/character-form';
-import {CharacterDetail} from './pages/character-detail/character-detail';
-import {BattleList} from './pages/battle-list/battle-list';
-import {CombatDisplay} from './pages/combat-display/combat-display';
-
+import {ROUTER_PROVIDERS, 
+				ROUTER_DIRECTIVES, 
+				RouteConfig}				from 'angular2/router';
+import {APP_BASE_HREF} 			from 'angular2/platform/common';
+import {bootstrap} 					from 'angular2-meteor-auto-bootstrap';
+import {Navigation} 				from './shared/navigation/navigation';
+import {DiceHelper} 				from './shared/dice-helper/dice-helper';
+import {CommandPalette} 		from './shared/command-palette/command-palette';
+import {HomePage} 					from './pages/home-page/home-page';
+import {CampaignList} 			from './pages/campaign-list/campaign-list';
+import {CampaignForm} 			from './pages/campaign-form/campaign-form';
+import {ContentCreator} 		from './pages/content-creator/content-creator';
+import {CharacterList} 			from './pages/character-list/character-list';
+import {CharacterForm} 			from './pages/character-form/character-form';
+import {CharacterDetail} 		from './pages/character-detail/character-detail';
+import {BattleList} 				from './pages/battle-list/battle-list';
+import {CombatDisplay} 			from './pages/combat-display/combat-display';
 @Component({
   selector: 'app',
 	template: `
